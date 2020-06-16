672eb78a93ac4d2c822d0ff86dc9e3d26bf0ff4d
Dylan McClain <dmcclain@paydici.com>
2020-06-16 16:34:14 -0700

    add images and get cells to display images at random

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="cell" :id="`cell_${id}`">{{ id }}</div>
+  <div class="cell" :id="`cell_${id}`">
+    <img :src="getImg(value)" class="tile-image" />
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,32 @@ export default {
 
   props: {
     id: { type: Number, required: true }
+  },
+
+  data: function() {
+    return {
+      value: 0
+    };
+  },
+
+  methods: {
+    getRandomInt: function(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    },
+
+    getImg(num) {
+      return require("../assets/images/" + num + ".png")
+    }
+  },
+
+  created() {
+    this.value = this.getRandomInt(1, 6);
   }
 };
 </script>
+
+<style scoped>
+.tile-image {
+  width: 100%;
+}
+</style>
