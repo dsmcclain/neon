a1c2b490bde2c11685b00087f4fc8a0609f47921
Dylan McClain <dmcclain@paydici.com>
2020-06-16 14:46:44 -0700

    create cell component

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,13 +1,16 @@
 <template>
-<div class="board-wrapper">
-  <div v-for="num in 81" :class="`cell-wrapper-${num}`" :key="num">
-    {{ num }}
+  <div class="board-wrapper">
+    <div v-for="num in 81" :class="`cell-wrapper-${num}`" :key="num">
+      <Cell :id="num"></Cell>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
+import Cell from "./Cell.vue";
+
 export default {
-  name: "Board"
+  name: "Board",
+  components: { Cell }
 };
 </script>
---
+++ src/components/Cell.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="cell" :id="`cell_${id}`">{{ id }}</div>
+</template>
+
+<script>
+export default {
+  name: "Board",
+
+  props: {
+    id: { type: Number, required: true }
+  }
+};
+</script>
