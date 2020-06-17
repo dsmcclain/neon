c32ae78ae7d6e6efae673d32baebf75266800a9e
Dylan McClain <dmcclain@paydici.com>
2020-06-16 21:02:45 -0700

    store cell values in vuex

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "Board",
 
@@ -18,18 +20,25 @@ export default {
     };
   },
 
+  computed: {
+    ...mapGetters(["allCells"])
+  },
+
   methods: {
+    ...mapActions(["setCell"]),
+
     getRandomInt: function(min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
     },
 
     getImg(num) {
-      return require("../assets/images/" + num + ".png")
+      return require("../assets/images/" + num + ".png");
     }
   },
 
   created() {
     this.value = this.getRandomInt(1, 6);
+    this.setCell({ id: this.id, value: this.value });
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,27 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const state = {
+  cells: []
+};
+
+const getters = {
+  allCells: state => state.cells
+};
+
+const actions = {
+  setCell({ commit }, cell) {
+    commit("setCell", cell);
+  }
+};
+
+const mutations = {
+  setCell: (state, cell) => state.cells.push(cell)
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state,
+  mutations,
+  actions,
+  getters
 });
