3610eb96989b8186bf8c0ebaa13506f35576f21a
Dylan McClain <dmcclain@paydici.com>
2020-09-22 08:35:08 -0700

    mobile welcome-screen

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="container">
-    <Console></Console>
-    <Board></Board>
+  <div>
+    <div class="mobile-screen">
+      <img src="./assets/images/neon-wave.png" />
+      <h2>Hi! Thank you for checking out Neon!</h2>
+      <hr class="separator-line">
+      <h3>
+        This game is designed for a larger screen. It is not playable on a
+        mobile device or tablet.
+      </h3>
+      <h3>
+        Please visit with a larger screen. I would love to show you around!
+      </h3>
+      <h3 class="signoff-message">
+        (In the meantime, you can check out more of my work
+        <a class="blog-link" href="https://dsmcclain.github.io">here</a>)
+      </h3>
+    </div>
+    <div class="container">
+      <Console></Console>
+      <Board></Board>
+    </div>
   </div>
 </template>
 
